76038b89afba0f1fef6758350a426dc5ca75b4b4
zukeke <1670386358@qq.com>
2019-09-29 16:33:01 +0800

    zoukeke

---
+++ HenanEnergy/HenanEnergy/HenanEnergyDevelop/EconomyEnergyRelationship.xaml.cs
@@ -0,0 +1,69 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HenanEnergy.HenanEnergyDevelop
+{
+    /// <summary>
+    /// EconomyEnergyRelationship.xaml 的交互逻辑
+    /// </summary>
+    public partial class EconomyEnergyRelationship : UserControl
+    {
+        public EconomyEnergyRelationship()
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "地区生产总值增速",
+                    Values = new ChartValues<double> { 0.10, 0.50, 0.39, 0.50 , 0.78, 0.35, 0.26, 0.11 }
+                    
+                }
+            };
+
+            //adding series will update and animate the chart automatically
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "能源消费增速",
+                Values = new ChartValues<double> { 0.11, 0.56, 0.42, 0.78, 0.35, 0.26, 0.11, 0.65 }
+               
+            });
+
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "规上企业用电增速",
+                Values = new ChartValues<double> { 0.56, 0.42, 0.78, 0.35, 0.11, 0.56, 0.42, 0.50 }
+              
+            });
+
+            //also adding values updates and animates the chart automatically
+            //SeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "2011", "2012", "2013", "2014","2015", "2016", "2017", "2018" };
+            //Formatter = value => value.ToString("N");
+
+            Formatter = value => value.ToString("P");
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+    }
+}
---
+++ HenanEnergy/HenanEnergy/HenanEnergyDevelop/ThreeIndex.xaml.cs
@@ -0,0 +1,63 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HenanEnergy.HenanEnergyDevelop
+{
+    /// <summary>
+    /// ThreeIndex.xaml 的交互逻辑
+    /// </summary>
+    public partial class ThreeIndex : UserControl
+    {
+        public ThreeIndex()
+        {
+            InitializeComponent();
+            Values1 = new ChartValues<ObservableValue>
+            {
+                new ObservableValue(3),
+                new ObservableValue(4),
+                new ObservableValue(6),
+                new ObservableValue(3),
+                new ObservableValue(2),
+                new ObservableValue(6)
+            };
+            Values2 = new ChartValues<ObservableValue>
+            {
+                new ObservableValue(5),
+                new ObservableValue(3),
+                new ObservableValue(5),
+                new ObservableValue(7),
+                new ObservableValue(3),
+                new ObservableValue(9)
+            };
+            Values3 = new ChartValues<ObservableValue>
+            {
+                new ObservableValue(7),
+                new ObservableValue(5),
+                new ObservableValue(3),
+                new ObservableValue(5),
+                new ObservableValue(3),
+                new ObservableValue(9)
+            };
+
+            DataContext = this;
+        }
+
+        public ChartValues<ObservableValue> Values1 { get; set; }
+        public ChartValues<ObservableValue> Values2 { get; set; }
+        public ChartValues<ObservableValue> Values3 { get; set; }
+    }
+}
